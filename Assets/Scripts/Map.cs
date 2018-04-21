97f6bd6d11dd27344bec0e976cf736371790814a
Julien R <julrousseau20@gmail.com>
2018-04-20 23:25:36 -0400

    20-23:25

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Map {
+
+    private static Vector2Int mapSize;
+
+    public static int Width { get { return mapSize.x; } }
+    public static int Height { get { return mapSize.y; } }
+
+    public static void SetSize(int x, int y)
+    {
+        mapSize = new Vector2Int(x, y);
+    }
+    
+}
---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    //----------------------------------------------------------------------------------------------------
+    // Create the map
+    //----------------------------------------------------------------------------------------------------
+    private void GenerateNewMap()
+    {
+        Map.SetSize(10, 10);
+
+        for (int x = 0; x < Map.Width; x++)
+        {
+            for (int y = 0; y < Map.Height; y++)
+            {
+                Debug.Log(Mathf.PerlinNoise(x, y));
+            }
+        }
+    }
+}
