9da6b9eb5c8fb1eeb3bf0d6cf5a6659a55997341
Julien R <julrousseau20@gmail.com>
2018-04-21 16:03:18 -0400

    ld41 au complet

---
+++ LD41_julien_00/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    private Player player;
+
+    public Vector3 offsetToPlayer = new Vector3(0, 8f, -7.5f);
+    public Vector3 angle = new Vector3(50, 0, 0);
+
+    private void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    //----------------------------------------------------------------------------------------------------
+    // Follow the player
+    //----------------------------------------------------------------------------------------------------
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + offsetToPlayer;
+        // TODO: Camera in a box to make it more realistic
+    }
+}
