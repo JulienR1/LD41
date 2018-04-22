61b0f51fbc578b7a09eba20e6f8681a29310dc03
LilTdah <dramsam123@hotmail.ca>
2018-04-21 21:30:30 -0400

    allo

---
+++ Assets/Scripts/spawnmanager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnmanager : MonoBehaviour {
+
+    public Enemy enemy;
+
+    public float spawnTime = 0;
+
+    private Vector3 spawnPosition;
+
+	
+	void Update () {
+
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+		
+	}
+	
+	void Spawn () {
+
+        spawnPosition.x = Random.Range(0, Map.Width);
+
+        spawnPosition.y = Map.Height;
+
+        spawnPosition.z = Random.Range(0, Map.Width);
+
+        if (Map.GetFromPosition(spawnPosition).Type == ObstacleInfo.ObstacleType.AIR) {
+            Instantiate(enemy, spawnPosition, Quaternion.identity);
+        }
+
+        
+
+
+
+    }
+}
